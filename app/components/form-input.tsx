35f87fce3b42192d418170677211c6cb6faf3a37
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-07-19 00:52:43 +0900

    Refactor FormInput component to accept additional props

--- app/components/form-input.tsx
+++ app/components/form-input.tsx
@@ -1,28 +1,26 @@
+import { InputHTMLAttributes } from 'react';
+
 interface FormInputProps {
   name: string;
-  type: string;
-  placeholder: string;
-  required: boolean;
-  errors: string[];
+  errors?: string[];
+  defaultValue?: string;
 }
 
 export default function FormInput({
   name,
-  type,
-  placeholder,
-  required,
   errors,
-}: FormInputProps) {
+  defaultValue,
+  ...props
+}: FormInputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="flex flex-col gap-2">
       <input
         className="bg-transparent rounded-md w-full h-10 p-2 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none  placeholder:text-neutral-400"
         name={name}
-        type={type}
-        placeholder={placeholder}
-        required={required}
+        defaultValue={defaultValue}
+        {...props}
       />
-      {errors.map((error, index) => (
+      {errors?.map((error, index) => (
         <span key={index} className="text-red-500 text-sm">
           {error}
         </span>
