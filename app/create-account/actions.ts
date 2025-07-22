073a33d6aec0327d525a5f72ae80906bad49dc47
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-07-22 22:29:01 +0900

    Using superRefine for email and username checks in the form schema

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -11,42 +11,53 @@ import bcrypt from 'bcrypt';
 import { redirect } from 'next/navigation';
 import { getSession } from '@/lib/session';
 
-const checkUsername = async (username: string) => {
-  const existingUser = await db.user.findUnique({
-    where: { username },
-    select: { id: true },
-  });
-  return !Boolean(existingUser);
-};
-
-const checkEmail = async (email: string) => {
-  const existingEmail = await db.user.findUnique({
-    where: { email },
-    select: { id: true },
-  });
-  return Boolean(existingEmail) === false;
-};
-
 const formSchema = z
   .object({
     username: z
       .string()
       .min(5, 'Username must be at least 5 characters long')
-      .trim()
-      .refine(checkUsername, 'Username is already taken'),
-    email: z
-      .string()
-      .email({ message: 'Invalid email format' })
-      .trim()
-      .refine(
-        checkEmail,
-        'There is an account already registered with that email.'
-      ),
+      .trim(),
+    email: z.string().email({ message: 'Invalid email format' }).trim(),
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH, 'Password must be at least 10 characters long')
       .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
     confirmPassword: z.string(),
+  })
+
+  .superRefine(async ({ email }, ctx) => {
+    const user = await db.user.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Username is already taken',
+        path: ['username'],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
+  })
+  .superRefine(async ({ email }, ctx) => {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+      },
+    });
+    if (user) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'This email is already taken',
+        path: ['email'],
+        fatal: true,
+      });
+      return z.NEVER;
+    }
   })
   .refine(
     ({ password, confirmPassword }) =>
