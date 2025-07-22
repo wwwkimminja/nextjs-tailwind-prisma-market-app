4dca896546cc02fd63fb8b54f110c3370a6baeb9
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-07-22 21:33:39 +0900

    Add user registration functionality with username and email validation, password hashing, and session management.

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,26 +1,47 @@
 'use server';
 
 import {
-  EMAIL_REGEX,
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from '@/lib/constants';
+import db from '@/lib/db';
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import { getIronSession } from 'iron-session';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+const checkUsername = async (username: string) => {
+  const existingUser = await db.user.findUnique({
+    where: { username },
+    select: { id: true },
+  });
+  return !Boolean(existingUser);
+};
+
+const checkEmail = async (email: string) => {
+  const existingEmail = await db.user.findUnique({
+    where: { email },
+    select: { id: true },
+  });
+  return Boolean(existingEmail) === false;
+};
 
 const formSchema = z
   .object({
     username: z
       .string()
       .min(5, 'Username must be at least 5 characters long')
-      .trim(),
+      .trim()
+      .refine(checkUsername, 'Username is already taken'),
     email: z
       .string()
       .email({ message: 'Invalid email format' })
       .trim()
       .refine(
-        email => EMAIL_REGEX.test(email),
-        'Only @zod.com emails are allowed'
+        checkEmail,
+        'There is an account already registered with that email.'
       ),
     password: z
       .string()
@@ -45,7 +66,7 @@ export const createAccount = async (prevState: any, formData: FormData) => {
     confirmPassword: formData.get('confirmPassword'),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
 
   if (!result.success) {
     return {
@@ -53,8 +74,29 @@ export const createAccount = async (prevState: any, formData: FormData) => {
       data: data,
     };
   }
-  await new Promise(resolve => setTimeout(resolve, 3000));
-  return {
-    result: 'OK',
-  };
+
+  const hashedPassword = await bcrypt.hash(result.data.password, 12);
+
+  const user = await db.user.create({
+    data: {
+      username: result.data.username,
+      email: result.data.email,
+      password: hashedPassword,
+    },
+    select: {
+      id: true,
+    },
+  });
+  console.log(user);
+
+  //log the user in
+  const cookie = await getIronSession(await cookies(), {
+    cookieName: 'carrot-market-reloaded',
+    password: process.env.COOKIE_PASSWORD!,
+  });
+  //@ts-ignore
+  cookie.id = user.id;
+  await cookie.save();
+
+  redirect('/profile');
 };
