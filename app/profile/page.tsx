8fc6efcb3839a44384ee93fbf6bac22f01210de2
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-07-23 19:31:04 +0900

    Add middleware  and profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,37 @@
-export default function Profile() {
-  return <div>Profile</div>;
+import db from '@/lib/db';
+import { getSession } from '@/lib/session';
+import { notFound, redirect } from 'next/navigation';
+
+async function getUser() {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+export default async function Profile() {
+  const user = await getUser();
+
+  const logout = async () => {
+    'use server';
+    const session = await getSession();
+    await session.destroy();
+    redirect('/');
+  };
+
+  return (
+    <div>
+      <h1>Welcome!{user?.username}</h1>
+      <form action={logout}>
+        <button type="submit">Log out</button>
+      </form>
+    </div>
+  );
 }
---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { getSession } from '@/lib/session';
+import { NextRequest, NextResponse } from 'next/server';
+
+const publicRoutes = ['/', '/login', '/sms', '/create-account'];
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+
+  const isPublicRoute = publicRoutes.includes(request.nextUrl.pathname);
+
+  if (!session.id) {
+    if (!isPublicRoute) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  } else {
+    if (isPublicRoute) {
+      return NextResponse.redirect(new URL('/products', request.url));
+    }
+  }
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
