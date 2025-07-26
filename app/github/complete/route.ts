bc7b7b15e4c76f924f85947205bd8bbe1ef2e4fb
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-07-26 13:10:50 +0900

    Implement GitHub OAuth user profile retrieval

--- app/github/complete/route.ts
+++ app/github/complete/route.ts
@@ -1,5 +1,12 @@
+import db from '@/lib/db';
 import { notFound } from 'next/navigation';
 import { NextRequest, NextResponse } from 'next/server';
+import {
+  getGitHubAccessToken,
+  getGitHubUserEmail,
+  getGitHubUserProfile,
+} from '@/lib/github';
+import { findUserByGithubId, loginUser } from '@/lib/auth';
 
 export async function GET(request: NextRequest) {
   const code = request.nextUrl.searchParams.get('code');
@@ -8,27 +15,45 @@ export async function GET(request: NextRequest) {
     return notFound();
   }
 
-  const acessTokenParams = new URLSearchParams({
-    client_id: process.env.GITHUB_CLIENT_ID!,
-    client_secret: process.env.GITHUB_CLIENT_SECRET!,
-    code,
-  }).toString();
+  try {
+    const accessToken = await getGitHubAccessToken(code);
+    const { id, avatar_url, login } = await getGitHubUserProfile(accessToken);
 
-  const accessTokenURL = `https://github.com/login/oauth/access_token?${acessTokenParams}`;
+    const email = await getGitHubUserEmail(accessToken);
 
-  const response = await fetch(accessTokenURL, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-    },
-  });
+    const user = await findUserByGithubId(id);
+    const sameUserName = await db.user.findUnique({
+      where: {
+        username: login,
+      },
+      select: {
+        id: true,
+      },
+    });
 
-  const accessTokenData = await response.json();
+    if (user) {
+      await loginUser(user.id);
+      return NextResponse.redirect(new URL('/profile', request.url));
+    }
 
-  if ('error' in accessTokenData) {
-    console.error(accessTokenData);
+    const newUser = await db.user.create({
+      data: {
+        github_id: id,
+        avatar: avatar_url,
+        username: sameUserName ? login + id : login,
+        email,
+        password: '',
+      },
+      select: {
+        id: true,
+      },
+    });
+
+
+    await loginUser(newUser.id);
+    return NextResponse.redirect(new URL('/profile', request.url));
+  } catch (error) {
+    console.error(error);
     return new Response(null, { status: 400 });
   }
-
-  return Response.json({ accessTokenData });
 }
