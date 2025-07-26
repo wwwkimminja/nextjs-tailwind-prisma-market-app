d756c75810b9f70e7a67a8078e14a55838802ec9
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-07-26 14:22:19 +0900

    Add SMS login functionality with token generation and validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -3,6 +3,9 @@
 import { z } from 'zod';
 import validator from 'validator';
 import { redirect } from 'next/navigation';
+import db from '@/lib/db';
+import crypto from 'crypto';
+import { loginUser } from '@/lib/auth';
 
 const phoneSchema = z
   .string()
@@ -12,17 +15,46 @@ const phoneSchema = z
     'Wrong phone format'
   );
 
-const tokenSchema = z.coerce.number().int().min(100000).max(999999);
+async function tokenExists(token: number) {
+  const exist = await db.sMSToken.findUnique({
+    where: {
+      token: token.toString(),
+    },
+  });
+  return Boolean(exist);
+}
+const tokenSchema = z.coerce
+  .number()
+  .int()
+  .min(100000)
+  .max(999999)
+  .refine(tokenExists, 'Invalid token');
 
 interface ActionState {
   token: boolean;
+}
+
+async function generateToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exist = await db.sMSToken.findUnique({
+    where: {
+      token: token,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (exist) {
+    return generateToken();
+  }
+  return token;
 }
 
 export const smsLogin = async (prevState: ActionState, formData: FormData) => {
   const phone = formData.get('phone');
   const token = formData.get('token');
 
-  if (prevState.token === false) {
+  if (!prevState.token) {
     const result = phoneSchema.safeParse(phone);
     if (!result.success) {
       return {
@@ -30,17 +62,62 @@ export const smsLogin = async (prevState: ActionState, formData: FormData) => {
         error: result.error.flatten(),
       };
     }
+    await db.sMSToken.deleteMany({
+      where: {
+        user: {
+          phone: result.data,
+        },
+      },
+    });
+
+    const token = await generateToken();
+    await db.sMSToken.create({
+      data: {
+        token,
+        user: {
+          connectOrCreate: {
+            where: {
+              phone: result.data,
+            },
+            create: {
+              username: crypto.randomBytes(10).toString('hex'),
+              phone: result.data,
+            },
+          },
+        },
+      },
+    });
+
     return {
       token: true,
     };
   } else {
-    const result = tokenSchema.safeParse(token);
+    const result = await tokenSchema.safeParseAsync(token);
     if (!result.success) {
       return {
         token: true,
         error: result.error.flatten(),
       };
+    } else {
+      const token = await db.sMSToken.findUnique({
+        where: {
+          token: result.data.toString(),
+        },
+        select: {
+          id: true,
+          user_id: true,
+        },
+      });
+
+      if (token) {
+        await loginUser(token.user_id);
+        await db.sMSToken.delete({
+          where: {
+            id: token.id,
+          },
+        });
+      }
+      redirect('/profile');
     }
-    redirect('/');
   }
 };
