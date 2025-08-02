182e40bed9a18cc1ef2437370fc9ced1bec52f9e
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2025-08-02 16:56:17 +0900

    Fix issue where a modal appears when navigating to the products/add page.

--- app/(tabs)/home/@modal/(...)products/[id]/page.tsx
+++ app/(tabs)/home/@modal/(...)products/[id]/page.tsx
@@ -1,7 +1,14 @@
 'use client';
 
 import ProductModal from '@/components/product-modal';
+import { useEffect } from 'react';
 
 export default function Modal({ params }: { params: { id: string } }) {
-  return <ProductModal productId={params.id} />;
+  const isAdd = params.id === 'add';
+  useEffect(() => {
+    if (isAdd) {
+      window.location.reload();
+    }
+  }, [params.id]);
+  return isAdd ? null : <ProductModal productId={params.id} />;
 }
